14964b9e2066c6c7dcd58dc6c2691a4cbe38da90
jschnei <jschneider2013@gmail.com>
2022-07-15 15:58:18 -0400

    Initial commit (moving square)

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), 
+                               Input.GetAxis("Vertical"), 
+                               0);
+        transform.position += Time.deltaTime * move * speed;
+    }
+}
+
+
